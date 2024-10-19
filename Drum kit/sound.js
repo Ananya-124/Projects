39ee76df0a6a2adc5dd1b689c9b4e33e2787ef2c
Ananya <ananyaananyaa124@gmail.com>
2024-10-19 12:12:44 +0530

    new

--- Drum kit/sound.js
+++ Drum kit/sound.js
@@ -1,7 +1,72 @@
-var noButtons = document.querySelectorAll(".drum").length;
-for(i=0;i<=noButtons;i++){
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
-function handleclick(){
-    alert("I got clicked");
-}}
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+    document.querySelectorAll(this.innerHTML.style.color = "blue");
+
+    
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+     
+     return "i am tired";
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+
