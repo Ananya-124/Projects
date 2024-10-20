e055f73333669e8b6a92fed3092a7d387ff38f33
Ananya <ananyaananyaa124@gmail.com>
2024-10-20 11:13:13 +0530

    new

--- Drum kit/sound.js
+++ Drum kit/sound.js
@@ -7,9 +7,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
-    document.querySelectorAll(this.innerHTML.style.color = "blue");
 
-    
+    buttonAnimation(buttonInnerHTML);
 
   });
 
@@ -19,7 +18,7 @@ document.addEventListener("keypress", function(event) {
 
   makeSound(event.key);
 
-  
+  buttonAnimation(event.key);
 
 });
 
@@ -28,8 +27,8 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-     
-     return "i am tired";
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
 
     case "a":
@@ -69,4 +68,14 @@ function makeSound(key) {
 }
 
 
+function buttonAnimation(currentKey) {
 
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
