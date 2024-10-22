9801b1bff5f007ddf978637329e58872b2bba596
Ananya <ananyaananyaa124@gmail.com>
2024-10-22 20:43:41 +0530

    new

--- Simon game/game.js
+++ Simon game/game.js
@@ -1,12 +1,11 @@
-$(".main").click(function(){
-    var boxinnerhtml = this.main;
-    anime(boxinnerhtml);
-})
-function anime(currentbox) {
-    var activebox = $("."+currentbox);
-    activebox.addClass("new1")
-    setTimeout(function () {
-        activebox.removeClass("new1")
-        
-    }, 1000);
+var buttonColours = ["red", "green", "yellow", "blue"];
+var gamepat = [];
+function nextSequence() {
+  var randNumber = Math.floor(Math.random() * 4);
+  var randcolor = buttonColours[randNumber];
+  gamepat.push(randcolor);
+  $("." + randcolor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio1 = new Audio("Projects\Simon game\sounds\blue.mp3" + randcolor);
+  audio1.play();
 }
+nextSequence();
